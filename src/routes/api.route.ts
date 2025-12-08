a274c8c691701774070336adb8d8422b0959c5cc
xavierdaniel1997 <danielcx532@gmail.com>
2025-12-08 20:44:35 +0530

    create the api for products

--- src/routes/api.route.ts
+++ src/routes/api.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
 import authRoute from "./auth.route";
-
+import productRoute from './product.route';
 
 const router = express.Router();
 
 router.use("/auth", authRoute);
+router.use("/product", productRoute)
 
 export default router;
