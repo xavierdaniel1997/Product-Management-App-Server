80aab44675ac72cab331e65a5bfb39c02ad1ef9c
xavierdaniel1997 <danielcx532@gmail.com>
2025-12-08 23:24:52 +0530

    update the get product api

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -6,7 +6,7 @@ import { upload } from '../middleware/multer';
 const router = express.Router();
 
 router.post("/add-product", isAuth, isAdmin, upload.array("images", 5), addProductController)
-router.get("/all-products", isAuth, getProductsController)
+router.get("/all-products", getProductsController)
 router.get("/product-details/:productId", isAuth, getProductByIdController)
 router.put("/update-product/:productId", isAuth, isAdmin,  upload.array("images", 5), updateProductController)
 router.delete("/delete-product/:productId", isAuth, isAdmin, deleteProductController)
